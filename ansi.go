ba60960d66cca513f96a1b3e970b9a5c665fb055
Eli Ribble <eli@gleipnir.technology>
2026-02-20 14:35:52 +0000

    More consistently create color
    I'm trying to figure out why the color is so far off, so much uglier,
    after parsing and translating the escape sequences...

--- ansi.go
+++ ansi.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gdamore/tcell/v3"
+	"github.com/gdamore/tcell/v3/color"
 	"github.com/leaanthony/go-ansi-parser"
 
 	"github.com/rs/zerolog/log"
@@ -12,7 +13,6 @@ func DrawStyledText(s tcell.Screen, x, y int, text []*ansi.StyledText) {
 	col := x
 	row := y
 	for _, seg := range text {
-		log.Debug().Str("text", seg.Label).Send()
 		style := convertStyle(seg)
 		for _, r := range seg.Label {
 			s.SetContent(col, row, r, nil, style)
@@ -32,24 +32,25 @@ func ParseANSI(buf []byte) ([]*ansi.StyledText, error) {
 }
 
 func convertStyle(t *ansi.StyledText) tcell.Style {
-	if t == nil || t.FgCol == nil {
+	background_c := tcell.ColorBlack
+	foreground_c := tcell.ColorWhite
+	if t == nil {
 		return tcell.StyleDefault
 	}
-	result := tcell.StyleDefault.Foreground(
-		tcell.NewRGBColor(
+	if t.FgCol != nil {
+		foreground_c = color.NewRGBColor(
 			int32(t.FgCol.Rgb.R),
 			int32(t.FgCol.Rgb.G),
 			int32(t.FgCol.Rgb.B),
-		),
-	)
-	if t.BgCol == nil {
-		return result
+		)
 	}
-	return result.Background(
-		tcell.NewRGBColor(
+	if t.BgCol != nil {
+		background_c = color.NewRGBColor(
 			int32(t.BgCol.Rgb.R),
 			int32(t.BgCol.Rgb.G),
 			int32(t.BgCol.Rgb.B),
-		),
-	)
+		)
+	}
+	result := tcell.StyleDefault.Foreground(foreground_c).Background(background_c)
+	return result
 }
